144c1d90c21250713919f9066c1193d13a27cb6d
DonnyTheTalker <maks.egorov.55@mail.ru>
2025-04-11 18:30:46 +0300

    WIP

---
+++ src/posts/kafka_producer.py
@@ -0,0 +1,27 @@
+# import json
+# import uuid
+# from datetime import datetime
+# from kafka import KafkaProducer
+# import os
+
+# KAFKA_PORT = os.environ.get('KAFKA_PORT', 9092)
+
+# class PostsEventProducer:
+#     def __init__(self):
+#         bootstrap_servers = [f'kafka:{KAFKA_PORT}']
+#         self.producer = KafkaProducer(
+#             bootstrap_servers=bootstrap_servers,
+#             value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+#             key_serializer=lambda k: str(k).encode('utf-8')
+#         )
+
+#     def send_view_event(self, client_id, entity_type, entity_id):
+#         event = {
+#             'event_id': str(uuid.uuid4()),
+#             'event_type': 'VIEW',
+#             'client_id': client_id,
+#             'timestamp': datetime.now().isoformat(),
+#             'entity_type': entity_type,
+#             'entity_id': entity_id
+#         }
+#         self.producer.send('content-views', key=client_id, value=event)
